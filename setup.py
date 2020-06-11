e17f2ee0097e6e15543f3b1480d3dc0439eae272
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2020-06-11 19:13:02 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,6 @@ from setuptools import setup
 import os
 
 
-def find_stubs(package):
-    stubs = []
-    for root, dirs, files in os.walk(package):
-        for file in files:
-            path = os.path.join(root, file).replace(package + os.sep, '', 1)
-            stubs.append(path)
-    return dict(package=stubs)
-
-
 setup(
     name='OCP-stubs',
     maintainer="OCP Developers",
@@ -19,5 +10,5 @@ setup(
     packages=['OCP-stubs'],
     # PEP 561 requires these
     install_requires=[],
-    package_data=find_stubs('OCP-stubs'),
+    include_package_data = True, #see MANIFEST
 )
